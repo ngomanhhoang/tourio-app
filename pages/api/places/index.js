e5658126f7b099cf8b574ce22515566ebf8c9cae
Hoang <manhhoangf1@gmail.com[D[C[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C>
2025-09-19 15:46:07 +0200

    Add: read data from database

--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,6 +1,13 @@
-import { places } from "../../../lib/db";
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Place";
 
-export default function handler(request, response) {
-  response.status(200).json(places);
-  return;
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find()
+    return response.status(200).json(places);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
